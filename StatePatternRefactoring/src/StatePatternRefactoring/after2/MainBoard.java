ba23772af20e3e1c0de5a9fefbb4a00fb18222ac
easyspubjava <eunjong.park@gmail.com>
2021-12-17 23:14:16 +0900

    state pattern refactoring

---
+++ StatePatternRefactoring/src/StatePatternRefactoring/after2/MainBoard.java
@@ -0,0 +1,17 @@
+package StatePatternRefactoring.after2;
+
+public class MainBoard {
+
+	public static void main(String[] args) {
+
+		Player player = new Player();
+		player.play(1);
+		AdvancedLevel aLevel = AdvancedLevel.getInstance();
+		player.upgradeLevel(aLevel);
+		player.play(2);
+		SuperLevel sLevel = SuperLevel.getInstance();
+		player.upgradeLevel(sLevel);
+		player.play(3);
+		
+	}
+}
---
+++ StatePatternRefactoring/src/StatePatternRefactoring/before/MainBoard.java
@@ -0,0 +1,15 @@
+package StatePatternRefactoring.before;
+
+public class MainBoard {
+
+	public static void main(String[] args) {
+
+		Player player = new Player();
+		player.play(1);
+		player.upgradeLevel();
+		player.play(2);
+		player.upgradeLevel();
+		player.play(3);
+		
+	}
+}
