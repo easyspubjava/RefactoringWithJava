7f10ace06f71a0e2348ff55b5473a8517bff2945
easyspubjava <eunjong.park@gmail.com>
2021-12-18 00:57:29 +0900

    introduce exception

--- IntroduceException/src/introduceexception/after/Password.java
+++ IntroduceException/src/introduceexception/after/Password.java
@@ -23,39 +23,4 @@ public class Password {
 			this.password = password;
 		}
 		
-		public static void main(String[] args) {
-
-			Password test = new Password();
-			String password = null;
-			try {
-				test.setPassword(password);
-				System.out.println("오류 없음1");
-			} catch (PasswordException e) {
-				System.out.println(e.getMessage());
-			}
-			
-			password = "abcd";
-			try {
-				test.setPassword(password);
-				System.out.println("오류 없음2");
-			} catch (PasswordException e) {
-				System.out.println(e.getMessage());
-			}
-			
-			password = "abcde";
-			try {
-				test.setPassword(password);
-				System.out.println("오류 없음3");
-			} catch (PasswordException e) {
-				System.out.println(e.getMessage());
-			}
-			
-			password = "abcde#1";
-			try {
-				test.setPassword(password);
-				System.out.println("오류 없음4");
-			} catch (PasswordException e) {
-				System.out.println(e.getMessage());
-			}
-		}
 }
